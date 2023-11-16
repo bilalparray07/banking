c93c0f71e7ee402fd03c64ad78c5ce68f55842bf
Bilal Ahmad <dev.renosoftwares@gmail.com>
2023-11-16 16:50:14 +0530

    dd

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv/config");
+require("dotenv").config();
 
 app.use(cors());
 app.options("*", cors());
